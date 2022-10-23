19bd25cf65f2cb5664c8c8c23a97a01123766a8d
Sachkov Ilya <sos31337@gmail.com>
2022-10-24 01:14:58 +0300

    Second step, added argparse parameter.

--- Gendiff/core.py
+++ Gendiff/core.py
@@ -5,6 +5,8 @@ def cmd():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument_group()
+    parser.add_argument('-f', '--format', help='set format of output')
     parser.parse_args()
 
 
